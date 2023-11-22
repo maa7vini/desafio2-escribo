e241f43310218cb6a97954c721ce846a52c1d73d
Matheus Vinicius <118569888+maa7vini@users.noreply.github.com>
2023-11-22 15:16:12 -0300

    Atualizando para porta dinâmica

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import connectUsers from './database/dataBase.js'
 
 export const SECRET = "KSDJAKDJ339492MDKMDF37"
 
+const port = process.env.PORT || 3001
+
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
 connectUsers().then(() => {
-    app.listen(3001, () => {
-        console.log('Site e Banco conectados http://localhost:3001/users')
+    app.listen(port, () => {
+        console.log(`Site e Banco conectados http://localhost:${port}/users`)
     })
 }).catch((err) => {
     console.log('Ops, algo deu errado!')
